fcde8ab00746104fc00f2d087040a3405353f85f
Güven Görgün <ggorgun@msn.com>
2024-12-24 21:03:30 +0300

    update homepage

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby + Netlify CMS Starter",
-    description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
+    title: `Müstesna Tasarım`,
+    author: `Esra Görgün`,
+    about: `Müstesna Tasarım, seramik sanatının inceliklerini ve estetiğini keşfetmek için kurulmuş bir platformdur. Burada, el yapımı seramik eserlerin yaratım sürecini, tekniklerini ve ilham kaynaklarını paylaşıyoruz.`,
+    description: `Çamurdan yapılan sanat`,
+    siteUrl: `https://mustesnatasarim.com/`,
   },
   plugins: [
     "gatsby-plugin-react-helmet",
---
+++ src/containers/home/intro/index.tsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import SocialProfile from '../../../components/social-profile/social-profile';
+import {
+  IntroWrapper,
+  IntroTitle,
+  Desciption,
+  HeroWrapper,
+  IntroContentWrapper,
+} from './style';
+import {
+  IoLogoFacebook,
+  IoLogoTwitter,
+  IoLogoInstagram,
+  IoLogoGithub,
+} from 'react-icons/io';
+import GatsbyImage from "../../../components/gatsby-image";
+
+type IntroProps = {};
+
+const SocialLinks = [
+  {
+    icon: <IoLogoFacebook />,
+    url: '#',
+    tooltip: 'Facebook',
+  },
+  {
+    icon: <IoLogoInstagram />,
+    url: '#',
+    tooltip: 'Instagram',
+  },
+  {
+    icon: <IoLogoTwitter />,
+    url: '#',
+    tooltip: 'Twitter',
+  },
+  {
+    icon: <IoLogoGithub />,
+    url: '#',
+    tooltip: 'Github',
+  },
+];
+
+const Intro: React.FunctionComponent<IntroProps> = (props) => {
+  const Data = useStaticQuery(graphql`
+    query {
+      heroImage: file(absolutePath: { regex: "/zho-small.png/" }) {
+        childImageSharp {
+          gatsbyImageData(
+              layout: FULL_WIDTH
+              placeholder: BLURRED
+              formats: [AUTO, WEBP, AVIF]
+          )
+        }
+      }
+      site {
+        siteMetadata {
+          author
+          about
+        }
+      }
+    }
+  `);
+
+  const { about } = Data.site.siteMetadata;
+
+  return (
+    <IntroWrapper>
+      {/*<BgText>ABOUT</BgText>*/}
+      <HeroWrapper>
+        <GatsbyImage src={Data.heroImage.childImageSharp.gatsbyImageData} alt="hero" />
+      </HeroWrapper>
+      <IntroContentWrapper>
+        <IntroTitle>Müstesna Tasarım</IntroTitle>
+        <Desciption>{about}</Desciption>
+        <SocialProfile items={SocialLinks} />
+      </IntroContentWrapper>
+    </IntroWrapper>
+  );
+};
+
+export default Intro;
